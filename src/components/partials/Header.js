1e76b5b3be8ce061eb032f7e0a89dfdab98258f9
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-10-06 17:22:19 +0000

    Initial Commit

---
+++ src/components/partials/Header.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header >
+            <nav >
+                <Link
+                    href="/"
+                >
+                    Home
+                </Link>
+                <Link
+                    href="/about"
+                >
+                    About
+                </Link>
+            </nav>
+        </header>
+    );
+}
